e31e30eb9baabb1a01f6831a271b4ba405a1f30b
Edward <edward821220@gmail.com>
2023-02-12 15:29:49 +0800

    finished layout and RWD

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,27 @@
-import '@/styles/globals.css'
-import type { AppProps } from 'next/app'
+import type { AppProps } from "next/app";
+import { createGlobalStyle } from "styled-components";
 
+const GlobalStyle = createGlobalStyle`
+  *,
+  *::before,
+  *::after {
+  margin: 0;
+  padding: 0;
+  box-sizing: inherit;
+}
+body {
+  box-sizing: border-box;
+  font-family: "Noto Serif", -apple-system, BlinkMacSystemFont, "Segoe UI", "Microsoft JhengHei", Roboto, "Helvetica Neue", Arial, sans-serif;
+}
+img {
+  height:auto
+}
+`;
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <>
+      <GlobalStyle />
+      <Component {...pageProps} />
+    </>
+  );
 }
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,9 +1,103 @@
-import Head from 'next/head'
-import Image from 'next/image'
-import { Inter } from '@next/font/google'
-import styles from '@/styles/Home.module.css'
+import Head from "next/head";
+import Image from "next/image";
+import styled from "styled-components";
+import PlaceHolder from "../../public/video-placeholder@desktop.png";
+import Pic1 from "../../public/section2-image1.png";
+import Pic2 from "../../public/section2-image2.png";
+import Pic3 from "../../public/section2-image3.png";
+import Pic4 from "../../public/section2-image4.png";
+import Pic5 from "../../public/section2-image5.png";
+import Pic6 from "../../public/section2-image6.png";
 
-const inter = Inter({ subsets: ['latin'] })
+const Banner = styled(Image)`
+  max-width: 100%;
+`;
+
+const Main = styled.main`
+  position: relative;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  height: 71.1111111vw;
+  @media screen and (max-width: 375px) {
+    height: 226.4vw;
+  }
+`;
+const Text = styled.p`
+  text-align: center;
+  font-size: 3.33333vw;
+  line-height: 4.5403vw;
+  @media screen and (max-width: 375px) {
+    font-size: 8.53333vw;
+    line-height: 11.6213333vw;
+  }
+`;
+const Image1 = styled(Image)`
+  position: absolute;
+  width: 28.681%;
+  left: 0;
+  top: 34.722222vw;
+  @media screen and (max-width: 375px) {
+    width: 55.2%;
+    left: -41.3333vw;
+    top: 132.26667vw;
+  }
+`;
+const Image2 = styled(Image)`
+  position: absolute;
+  width: 24.6528%;
+  left: 62.71vw;
+  top: 45.139vw;
+  @media screen and (max-width: 375px) {
+    width: 57.3333%;
+    left: 42.666667vw;
+    top: 155.73333vw;
+  }
+`;
+const Image3 = styled(Image)`
+  position: absolute;
+  width: 13.8889%;
+  left: 87.361111vw;
+  top: 30.555556vw;
+  @media screen and (max-width: 375px) {
+    width: 33.6%;
+    left: 92vw;
+    top: 72.266667vw;
+  }
+`;
+const Image4 = styled(Image)`
+  position: absolute;
+  width: 11.944%;
+  left: 69.1vw;
+  top: 10.4167vw;
+  @media screen and (max-width: 375px) {
+    width: 21.6%;
+    left: 70.13333vw;
+    top: 54.66667vw;
+  }
+`;
+const Image5 = styled(Image)`
+  position: absolute;
+  width: 16.04167%;
+  left: 3.47222vw;
+  top: 14.583333vw;
+  @media screen and (max-width: 375px) {
+    width: 34.13333%;
+    left: 0;
+    top: 58.93333vw;
+  }
+`;
+const Image6 = styled(Image)`
+  position: absolute;
+  width: 23.055555%;
+  left: 32.778vw;
+  top: 2.9861vw;
+  @media screen and (max-width: 375px) {
+    width: 40.5333333%;
+    left: 9.6vw;
+    top: 9.6vw;
+  }
+`;
 
 export default function Home() {
   return (
@@ -14,110 +108,27 @@ export default function Home() {
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      <main className={styles.main}>
-        <div className={styles.description}>
-          <p>
-            Get started by editing&nbsp;
-            <code className={styles.code}>src/pages/index.tsx</code>
-          </p>
-          <div>
-            <a
-              href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-              By{' '}
-              <Image
-                src="/vercel.svg"
-                alt="Vercel Logo"
-                className={styles.vercelLogo}
-                width={100}
-                height={24}
-                priority
-              />
-            </a>
-          </div>
-        </div>
-
-        <div className={styles.center}>
-          <Image
-            className={styles.logo}
-            src="/next.svg"
-            alt="Next.js Logo"
-            width={180}
-            height={37}
-            priority
-          />
-          <div className={styles.thirteen}>
-            <Image
-              src="/thirteen.svg"
-              alt="13"
-              width={40}
-              height={31}
-              priority
-            />
-          </div>
-        </div>
-
-        <div className={styles.grid}>
-          <a
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className={styles.card}
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <h2 className={inter.className}>
-              Docs <span>-&gt;</span>
-            </h2>
-            <p className={inter.className}>
-              Find in-depth information about Next.js features and&nbsp;API.
-            </p>
-          </a>
-
-          <a
-            href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className={styles.card}
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <h2 className={inter.className}>
-              Learn <span>-&gt;</span>
-            </h2>
-            <p className={inter.className}>
-              Learn about Next.js in an interactive course with&nbsp;quizzes!
-            </p>
-          </a>
-
-          <a
-            href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className={styles.card}
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <h2 className={inter.className}>
-              Templates <span>-&gt;</span>
-            </h2>
-            <p className={inter.className}>
-              Discover and deploy boilerplate example Next.js&nbsp;projects.
-            </p>
-          </a>
-
-          <a
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className={styles.card}
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <h2 className={inter.className}>
-              Deploy <span>-&gt;</span>
-            </h2>
-            <p className={inter.className}>
-              Instantly deploy your Next.js site to a shareable URL
-              with&nbsp;Vercel.
-            </p>
-          </a>
-        </div>
-      </main>
+      <Banner
+        src={PlaceHolder}
+        alt="placeholder"
+        object-fit="contain"
+        objectPosition="center"
+      ></Banner>
+      <Main>
+        <Text>
+          Creating perfect
+          <br />
+          lines and imposing
+          <br />
+          presence
+        </Text>
+        <Image1 src={Pic1} alt="image1"></Image1>
+        <Image2 src={Pic2} alt="image2"></Image2>
+        <Image3 src={Pic3} alt="image3"></Image3>
+        <Image4 src={Pic4} alt="image4"></Image4>
+        <Image5 src={Pic5} alt="image5"></Image5>
+        <Image6 src={Pic6} alt="image6"></Image6>
+      </Main>
     </>
-  )
+  );
 }
